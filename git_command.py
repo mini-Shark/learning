6bb057282e7aca8a36e75db8bf491f9e6bc0198a
mini-Shark <1094773235@qq.com>
2018-10-16 17:13:11 +0800

    add more command method

--- git_command.py
+++ git_command.py
@@ -10,7 +10,7 @@ git reset --hard HEAD^ : roll back to previous version, "HEAD" means current ver
 git reset --hard commit_id : jump to a special version that point by commit_id
 git reflog : disply the history of our command, could used to find a commit_id when we close terminal
 git diff HEAD -- readme.txt : check the difference between working directory and repository
-git checkout -- file.name : discard modify at working directory 
+git checkout -- file.name : discard modify at working directory, actuall it's use repository version to replace working directory version 
 git reset HEAD file.name : discard modify at stage and put it back to working directory 
-
+git rm file.name : delete file in repository 
 '''
