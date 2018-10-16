f4a7085469f8aff28fa72a596aac5d60d0ef9e3d
mini-Shark <1094773235@qq.com>
2018-10-16 22:39:42 +0800

    basic learning

--- tf.py
+++ tf.py
@@ -2,7 +2,3 @@ import tensorflow as tf
 import numpy as np 
 import SimpleITK as sitk 
 import glob
-#git tracks changed
-#some test for branch dev 
-#Creating a new branch is quick AND simple.
-# hhhhh test 
